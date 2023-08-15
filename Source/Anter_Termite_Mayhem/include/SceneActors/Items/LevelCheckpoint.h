83ee87d1c4de97a2dd7dcb783aa5bb170d528ec4
Matteo Mazzarini <matteo.mazzarini.91@gmail.com>
2023-08-15 11:25:30 +0200

    Implemented logics for LevelCheckpoint. Tested in PIE until broadcast.

--- Source/Anter_Termite_Mayhem/include/SceneActors/Items/LevelCheckpoint.h
+++ Source/Anter_Termite_Mayhem/include/SceneActors/Items/LevelCheckpoint.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+
 #include "LevelCheckpoint.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCheckpointActivatedDelegate, ALevelCheckpoint*, CurrentCheckpointPtr);
+
+//Base class for checkpoint. 
+//Behavior: if the checkpoint is triggered (trigger placeholder event: contact with Anter): the checkpoint broadcasts an OnActivated event
+//The broadcaster may be used to activate the checkpoint status
+//implementation otherwise is similar to LevelGoal
 
 UCLASS(Blueprintable,BlueprintType)
 class ANTER_TERMITE_MAYHEM_API ALevelCheckpoint : public AActor
@@ -13,8 +21,27 @@ class ANTER_TERMITE_MAYHEM_API ALevelCheckpoint : public AActor
 
 public:
 
-    ALevelCheckpoint(){}
+    ALevelCheckpoint();
+
+    void BeginPlay() override;
+
+    void SetBindings();
+
+    void OnCheckpointActivated();
 
     FCheckpointActivatedDelegate OnActivatedCheckpointDelegate;
+
+protected:
+
+    UPROPERTY(BlueprintReadOnly,VisibleAnywhere)
+    UStaticMeshComponent* LevelMesh;
+
+    UPROPERTY(BlueprintReadOnly,VisibleAnywhere)
+    UBoxComponent* LevelBox;
+
+    UFUNCTION()
+    void OnCollidedWithActor(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+    bool bIsActivated = false;
 
 };
--- Source/Anter_Termite_Mayhem/src/SceneActors/Items/LevelCheckpoint.cpp
+++ Source/Anter_Termite_Mayhem/src/SceneActors/Items/LevelCheckpoint.cpp
@@ -1,1 +1,40 @@
 #include "SceneActors/Items/LevelCheckpoint.h"
+#include "GameSpecificStaticLibrary/GameSpecificStaticLibrary.h"
+#include "Pawn/AnterPaperCharacter.h"
+
+ALevelCheckpoint::ALevelCheckpoint()
+{
+    LevelMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LevelMesh"));
+    LevelMesh->SetupAttachment(RootComponent);
+
+    LevelBox = CreateDefaultSubobject<UBoxComponent>(TEXT("LevelBox"));
+    LevelBox->SetupAttachment(RootComponent);
+}
+
+void ALevelCheckpoint::BeginPlay()
+{
+    Super::BeginPlay();
+    SetBindings();
+}
+
+void ALevelCheckpoint::OnCheckpointActivated()
+{
+    bIsActivated = true;
+    OnActivatedCheckpointDelegate.Broadcast(this);
+}
+
+void ALevelCheckpoint::SetBindings()
+{
+    if(LevelMesh !=nullptr)
+    {
+        LevelMesh->OnComponentBeginOverlap.AddDynamic(this,&ALevelCheckpoint::OnCollidedWithActor);
+    }
+}
+
+void ALevelCheckpoint::OnCollidedWithActor(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+    if(AAnterPaperCharacter* Anter = Cast<AAnterPaperCharacter>(OtherActor))
+    {
+        OnCheckpointActivated();
+    }
+}
