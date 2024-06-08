00faa5f90b80ba23f472fb44458cbc712451d2b2
Matteo Mazzarini <matteo.mazzarini.91@gmail.com>
2024-06-08 16:01:52 +0200

    Update MovingPlatform.cpp

--- Source/Anter_Termite_Mayhem/src/SceneActors/Platforms/MovingPlatform.cpp
+++ Source/Anter_Termite_Mayhem/src/SceneActors/Platforms/MovingPlatform.cpp
@@ -8,14 +8,6 @@ AMovingPlatform::AMovingPlatform()
     PrimaryActorTick.bTickEvenWhenPaused = true;
 }
 
-void AMovingPlatform::BeginPlay()
-{
-    Super::BeginPlay();
-    Anter = Cast<AAnterPaperCharacter>(UGameplayStatics::GetActorOfClass(this,AAnterPaperCharacter::StaticClass()));
-    OldPosition = GetActorLocation();
-    NewPosition = GetActorLocation();
-}
-
 void AMovingPlatform::Tick(float DeltaTime)
 {   
     Super::Tick(DeltaTime);
@@ -26,16 +18,16 @@ void AMovingPlatform::UpdateMovement()
 {
     OldPosition = NewPosition;
     NewPosition = GetActorLocation();
-    if(Anter != nullptr)
+    if(AnterRef.IsValid())
     {
-        TArray<TPair<AActor*,EPlatformCollisionType>> PlatformCollisions = Anter->GetResigsteredPlatformCollisions();
+        TArray<TPair<AActor*,EPlatformCollisionType>> PlatformCollisions = AnterRef->GetResigsteredPlatformCollisions();
         CandidateCollision = PlatformCollisions.FindByPredicate([this](TPair<AActor*,EPlatformCollisionType> TPAEColl)
         {
             return (TPAEColl.Key == this);
         });
         if(CandidateCollision != nullptr && CandidateCollision->Value == EPlatformCollisionType::IsVerticallyColliding)
         {
-            Anter->AddActorWorldOffset(NewPosition-OldPosition);
+            AnterRef->AddActorWorldOffset(NewPosition-OldPosition);
         }
     }
 }
@@ -44,7 +36,7 @@ void AMovingPlatform::Setup()
 {
     if(UMovingActorMovementSupportComponent* ActorMovement = FindComponentByClass<UMovingActorMovementSupportComponent>())
     {
-        Anter = Cast<AAnterPaperCharacter>(UGameplayStatics::GetActorOfClass(this,AAnterPaperCharacter::StaticClass()));
+        AnterRef = Cast<AAnterPaperCharacter>(UGameplayStatics::GetActorOfClass(this,AAnterPaperCharacter::StaticClass()));
         OldPosition = GetActorLocation();
         NewPosition = GetActorLocation();
         ActorMovement->ResetMovement();
